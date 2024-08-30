ecdef4713d5c36aa34c737ad6c6f8259238de075
DEvARyan77 <devaryan7766@gmail.com>
2024-08-30 23:52:18 +0530

    Try

--- Connections/Database.js
+++ Connections/Database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config()
-console.log(process.env.MONGODBURL);
 
 mongoose.connect(process.env.MONGODBURL,{dbName:'YourHR'}).then(()=>{
     console.log("Connected to Database")
