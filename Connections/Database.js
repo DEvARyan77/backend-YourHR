a0d38255e7a655b8eed64dceffdb3e8c85d0d9ca
DEvARyan77 <devaryan7766@gmail.com>
2024-08-30 23:43:28 +0530

    Update Database.js

--- Connections/Database.js
+++ Connections/Database.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config()
+console.log(process.env.MONGODBURL);
 
-mongoose.connect(process.env.MONGODB_URL,{dbName:'YourHR'}).then(()=>{
+mongoose.connect(process.env.MONGODBURL,{dbName:'YourHR'}).then(()=>{
     console.log("Connected to Database")
 }).catch((err)=>{
     console.log(err)
